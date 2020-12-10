1f62e4adee0b0a40c3eabb8f9f8f526379522617
bing <jingxiaobing@mininglamp.com>
2020-12-10 11:32:37 +0800

    change dumb-jump config to use xref

--- init.el
+++ init.el
@@ -237,12 +237,7 @@
          ("<mouse-2>" . mc/add-cursor-on-click)))
 
 (use-package dumb-jump
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-	 ("M-g b" . dumb-jump-back)
-         ("M-g i" . dumb-jump-go-prompt)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :init (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
   :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
   :ensure)
 
