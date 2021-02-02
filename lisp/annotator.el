22e581c3407c897cc5a798be6fb6d9dbc16edb5c
bing <jingxiaobing@gmail.com>
2021-02-02 14:47:01 +0800

    accurate click & some key mapping for annotator-mode

--- lisp/annotator.el
+++ lisp/annotator.el
@@ -47,6 +47,49 @@
 	 (2 font-lock-string-face)
 	 (3 'annotation-edge-face))))
 
+(defvar annotator-mode-map nil "Keymap for `annotator-mode'")
+
+(progn
+  (setq annotator-mode-map (make-sparse-keymap))
+  ;; ignore a-z, A-Z
+  (dolist (char (number-sequence ?a ?z))
+    (let ((lower (char-to-string char))
+	  (upper (char-to-string (- char 32))))
+      (define-key annotator-mode-map (kbd lower) 'ignore)
+      (define-key annotator-mode-map (kbd upper) 'ignore)))
+  ;; ignore 0-9
+  (dolist (char (number-sequence ?0 ?9))
+    (define-key annotator-mode-map
+      (kbd (char-to-string char)) 'ignore))
+  (define-key annotator-mode-map (kbd "q") 'ignore)
+  (define-key annotator-mode-map (kbd "Q") 'ignore))
+
+(defun adjust-point-after-click (event &optional _)
+  "Adjust point.
+Adjust point depending on which portion of the character the
+cursor clicked on, if on the right half, move point after.
+EVENT is the mouse event."
+  (let* ((posn (event-end event))
+         (x (car (posn-object-x-y posn)))
+         (w (car (posn-object-width-height posn))))
+    ;; ‘mouse-set-point’ is called twice when you click mouse, first
+    ;; in ‘down-mouse-1’, called by ‘mouse-drag-region’ ->
+    ;; ‘mouse-drag-track’ to set point, second in ‘mouse-1’, when
+    ;; mouse released and Emacs realized that this is a click event.
+    ;; We want to adjust point in both cases.
+    (when (and (null (posn-object posn))
+               (> x (/ w 2)))
+      (forward-char))))
+
+;; https://emacs-china.org/t/topic/13681/7
+(define-minor-mode accurate-click-mode
+  "Accurate point position on click."
+  :global t
+  :lighter ""
+  (if accurate-click-mode
+      (advice-add 'mouse-set-point :after #'adjust-point-after-click)
+    (advice-remove 'mouse-set-point #'adjust-point-after-click)))
+
 ;;;###autoload
 (define-derived-mode annotator-mode fundamental-mode "Annotator"
   "Major mode for Text Annotating"
@@ -54,7 +97,8 @@
   (setq font-lock-defaults '(entity-highlights))
   (setq line-spacing 5)
   (setq cursor-type 'bar)
-  (blink-cursor-mode 1))
+  (blink-cursor-mode 1)
+  (accurate-click-mode 1))
 
 (provide 'annotator)
 ;;; annotator.el ends here
